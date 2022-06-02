26c2c1db3ef05d7798dff3eb823c4f696eecd1ea
katman-Generation <tkatema5@gmail.com>
2022-06-02 14:20:48 -0700

    submitting

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
+    import calculator_1
+    str1 = "Usage: ./100-my_calculator.py <a> <operator> <b>"
+    str2 = "Unknown operator. Available operators: +, -, * and /"
+    num = (len(sys.argv))
 
-    num_args = len(sys.argv)
-    if num_args != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if num != 4:
+        print("{}".format(str1))
         exit(1)
+
+    if sys.argv[2] not in "+-*/":
+        print("{}".format(str2))
+        exit(1)
+
     a = int(sys.argv[1])
-    op = sys.argv[2]
     b = int(sys.argv[3])
 
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+    if sys.argv[2] == "+":
+        result = calculator_1.add(a, b)
+
+    if sys.argv[2] == "-":
+        result = calculator_1.sub(a, b)
+
+    if sys.argv[2] == "*":
+        result = calculator_1.mul(a, b)
+
+    if sys.argv[2] == "/":
+        result = calculator_1.div(a, b)
+    print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, result))
