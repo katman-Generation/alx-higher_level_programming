d9c01c5b506cd5e06343b3a2ff9f0d1e4ca9dc25
katman-Generation <tkatema5@gmail.com>
2022-06-04 14:43:22 -0700

    submitting

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace('c', '') + my_string.replace('C', '')
+    new_string = my_string.replace('cC', '')
     return new_string
