eb6034592c611d7e6f1eb15d7d6eea4f0dcf39f1
katman-Generation <tkatema5@gmail.com>
2022-06-21 15:16:59 -0700

    submitting

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module for task 0 of 0x06-Python-classes project -
+write an empty class Square that defines a square """
+
+class Square:
+    """class Square with private attribute size w prop and setter"""
+    def __init__(self, size=0):
+        """Initializes the data."""
+        self.__size = size
+
+        @property
+        def size(self):
+            """Retrieves the size."""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """Sets the size to a value."""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+            def area(self):
+                """Returns the current square area."""
+                return self.__size ** 2
