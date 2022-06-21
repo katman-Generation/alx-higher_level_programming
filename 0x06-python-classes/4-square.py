8b70cb8d70799be0f017cce4a0e69f43efd6cc44
katman-Generation <tkatema5@gmail.com>
2022-06-21 15:21:52 -0700

    submitting

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,12 +6,18 @@ class Square:
     """class Square with private attribute size w prop and setter"""
     def __init__(self, size=0):
         """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
 
         @property
         def size(self):
             """Retrieves the size."""
             return self.__size
+
 
         @size.setter
         def size(self, value):
@@ -21,6 +27,7 @@ class Square:
             elif value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
+
 
             def area(self):
                 """Returns the current square area."""
