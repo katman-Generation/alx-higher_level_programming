74188268fa612f4519f127b494cac4cd2acc3b15
katman-Generation <tkatema5@gmail.com>
2022-06-21 14:53:49 -0700

    submitting

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module for task 0 of 0x06-Python-classes project -
+write an empty class Square that defines a square """
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
