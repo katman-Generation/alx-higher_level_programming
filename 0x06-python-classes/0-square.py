cb9520dbd8d8c2da559987e365ae52e2cd3c69d9
katman-Generation <tkatema5@gmail.com>
2022-06-21 14:50:30 -0700

    submitting

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" Module for task 0 of 0x06-Python-classes project -
+write an empty class Square that defines a square """
+
+
 class Square:
+    """ an empty class that defines a square """
     pass
