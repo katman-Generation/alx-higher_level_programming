5b3cefcc2169178cfaf0d5172d37b9f90a2ac62c
katman-Generation <tkatema5@gmail.com>
2022-06-21 15:04:07 -0700

    submitting

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+
     def area(self):
         """Returns the current square area."""
         return self.__size ** 2
